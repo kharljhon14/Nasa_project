0ed3343dd564cefd6c2ae6958c3c3c87debb8a66
Karl <enriquezkharl14@gmail.com>
2022-03-31 03:28:20 +0800

    Loading data on startup

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const planetsRouter = require("./routes/planets/planets.router");
 
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+app.use(planetsRouter);
 module.exports = app;
