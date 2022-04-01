2f8cbc758db9377263a38274b22780c52a260b55
Karl <enriquezkharl14@gmail.com>
2022-04-02 00:38:47 +0800

    Post validations

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,11 +7,18 @@ function httpgetAllLaunches(req, res) {
 function httpaddNewLaunch(req, res) {
    const launch = req.body;
 
+   if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+      return res.status(400).json({ error: "Missing required property" });
+   }
+
    launch.launchDate = new Date(launch.launchDate);
+   if (isNaN(launch.launchDate)) {
+      return res.status(400).json({ error: "Invalid launch date" });
+   }
 
    addNewLaunch(launch);
    //201 = created
-   return res.status(201).json(launch); 
+   return res.status(201).json(launch);
 }
 
 module.exports = { httpgetAllLaunches, httpaddNewLaunch };
